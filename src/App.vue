ef98e87515308dc8908338a1b487958303b9535a
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-01-23 19:21:28 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <LabeledInput id="username" label-content="Username:" />
+  </div>
+</template>
+
+<script>
+import LabeledInput from './components/LabeledInput.vue';
+
+export default {
+  components: {
+    LabeledInput
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LabeledInput.vue
@@ -0,0 +1,31 @@
+<!-- src/components/LabeledInput.vue -->
+<template>
+  <div>
+    <label :for="id">{{ labelContent }}</label>
+    <input v-model="inputValue" :id="id" type="text">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    labelContent: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const inputValue = ref('');
+
+    return {
+      inputValue
+    };
+  }
+};
+</script>
